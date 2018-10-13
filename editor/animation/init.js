ee7974567589d891f93df7f7c1b5acb64903e3f0
Alexander Lyabah <a.lyabah@checkio.org>
2018-10-13 09:29:21 +0300

    init.js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -8,8 +8,8 @@ requirejs(['ext_editor_io', 'jquery_190'],
         var io = new extIO({
             multipleArguments: true,
             functions: {
-                python: 'sum_two',
-                js: 'sumTwo'
+                python: 'ryerson_letter_grade',
+                js: 'ryersonLetterGrade'
             }
         });
         io.start();
